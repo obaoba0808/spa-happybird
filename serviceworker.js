863ea7316931b4dbdb00128ccc0e08ef26454a6e
傳播妹 <obaoba0088@gmail.com>
2023-09-17 00:06:33 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -27,11 +27,3 @@ return networkResponse;
 );
 });
 
-importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
-
-AMP_SW.init(
-assetCachingOptions: [{
-regexp: /\.(png|jpg)/,
-cachingStrategy: 'CACHE_FIRST'
-}],
-);
