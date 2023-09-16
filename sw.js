2e4eb7b1935afabb25fee18f4ed4193ef8b8c797
傳播妹 <obaoba0088@gmail.com>
2023-09-16 23:57:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,38 +7,31 @@ const filesToCache = [
 ];
 
 //install the sw
-self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+
+self.addEventListener('fetch', function(event) {
+event.respondWith(
+caches.open('mysite').then(function(cache) {
+return cache.match(event.request).then(function(response) {
+var fetchPromise = fetch(event.request).then(function(networkResponse) {
+cache.put(event.request, networkResponse.clone());
+return networkResponse;
+})
+
+        // Modify the response here before it goes out..
+        ...
+
+        return response || fetchPromise;
+      })
     })
-  );
+
+);
 });
 
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 
-self.addEventListener('activate', function (e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
-
-
-self.addEventListener('fetch', function (e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+AMP_SW.init(
+assetCachingOptions: [{
+regexp: /\.(png|jpg)/,
+cachingStrategy: 'CACHE_FIRST'
+}],
+);
